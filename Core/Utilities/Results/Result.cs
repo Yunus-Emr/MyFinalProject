3b7659aaf1ee0e4234ca0395a9c8384709212591
YUNUS EMRE AKCA <2022212026@cumhuriyet.edu.tr>
2023-07-02 19:32:28 +0300

    Ders 12

--- Core/Utilities/Results/Result.cs
+++ Core/Utilities/Results/Result.cs
@@ -12,8 +12,7 @@ namespace Core.Utilities.Results
         /*ikisini birden çalıştırmak istesek böyle kullanırız nedeni ise programcı sadece true yada false vermek isteyebilir
           ama biz tek ctor ile yapsaydık o zaman hem bool hemde string ifade göndermek zorunda olurduk ama aşşağıdaki yapıda sadece true yada false kullanmak istesek
           this anahtar kelimesi ile üsteki result'ı çalıştırarak istersek hem mesaj bool yada sadece bool'u kullanabiliriz    
-         
-        Bu Yapı constructor'un en üst kullanım şekillerinden biridir.*/
+         */
         
         public Result(bool success,string message):this(success)
         {
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,5 +1,8 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 
@@ -18,9 +21,10 @@ namespace WebAPI
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Host.UseServiceProviderFactory(Services => new AutofacServiceProviderFactory()).ConfigureContainer<ContainerBuilder>(builder => { builder.RegisterModule(new AutofacBusinessModule());});
 
-            builder.Services.AddSingleton<IProductService, ProductManager>();
-            builder.Services.AddSingleton<IProductDal,EfProductDal>();
+            //builder.Services.AddSingleton<IProductService, ProductManager>();
+            //builder.Services.AddSingleton<IProductDal,EfProductDal>();
 
 
             var app = builder.Build();
